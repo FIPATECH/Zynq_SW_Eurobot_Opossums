80b336e1c7c4bf37bf9cad9d17afa50c70b309bd
MartH76 <martin25.husser@gmail.com>
2025-02-25 21:52:02 +0100

    add Asserv_type.h

---
+++ Eurobot_2025/src/lib_asserv/Asserv_type.h
@@ -0,0 +1,52 @@
+#ifndef ASSERV_TYPE_H
+#define ASSERV_TYPE_H
+
+/*****************************    Odometrie    *******************************/
+
+// Position absolue du robot (x, y, et theta)
+typedef struct {
+    float x; // en metre
+    float y; // en metre
+    float t; // en radian
+} Position;
+
+// Vitesse et vitesse angulaire du robot
+typedef struct {
+    float vx; // en m/s
+    float vy; // en m/s
+    float vt; // en rad/s
+} Speed;
+
+// acceleration du robot (dv/dt,  d2theta/dt2   et   v*(dtheta/dt))
+typedef struct {
+    float ax; // en m/s2
+    float ay; // en m/s2
+    float at; // en rad/s2
+} Acceleration;
+
+/**************************** PID  *****************************/
+typedef struct {
+    float kp;
+    float ki;
+    float kd;
+} PID_coef;
+
+typedef struct {
+    float err;
+    float err_int;
+    float err_der;
+} PID_err;
+
+typedef struct {
+    PID_coef coef;
+    PID_err err;
+} PID_pos;
+
+typedef struct {
+    PID_coef coef;
+    PID_err err1;
+    PID_err err2;
+    PID_err err3;
+} PID_speed;
+
+#endif
