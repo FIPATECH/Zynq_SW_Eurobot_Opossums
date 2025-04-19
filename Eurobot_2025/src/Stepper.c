8335b1e3c4042486c88b1f054a194d12832bd1f5
MartH76 <martin25.husser@gmail.com>
2025-04-19 17:04:50 +0200

    add initialization of stepper position

--- Eurobot_2025/src/Stepper.c
+++ Eurobot_2025/src/Stepper.c
@@ -4,32 +4,82 @@ STEPPER stepper_1;
 
 STEPPER stepper[NBR_STEPPER];
 
+// end stop switches
+XGpio high_switch_elevator;
+XGpio low_switch_elevator;
+int high_switch_elevator_state = 0;
+int low_switch_elevator_state = 0;
+
+
+int init_stepper_state = 0;
+
 void Init_Stepper(void){
-    // create a stepper object
-    stepper[0] = stepper_1;
-    // initialize the stepper object
-    XGpio_Initialize(&stepper_1.STEP,   STEP_DEVICE_ID);
-    XGpio_Initialize(&stepper_1.SPEED,  SPEED_DEVICE_ID);
-    XGpio_Initialize(&stepper_1.MODE,   MODE_DEVICE_ID);
-    XGpio_Initialize(&stepper_1.DIR,    DIR_DEVICE_ID);
-    XGpio_Initialize(&stepper_1.EN,     EN_DEVICE_ID);
-    XGpio_Initialize(&stepper_1.DONE,   DONE_DEVICE_ID);
-    XGpio_SetDataDirection(&stepper_1.STEP, 1, 0);
-    XGpio_SetDataDirection(&stepper_1.SPEED, 1, 0);
-    XGpio_SetDataDirection(&stepper_1.MODE, 1, 0);
-    XGpio_SetDataDirection(&stepper_1.DIR, 1, 0);
-    XGpio_SetDataDirection(&stepper_1.EN, 1, 0);
-    XGpio_SetDataDirection(&stepper_1.DONE, 1, 1);
-    stepper_1.step = 0; 
-    stepper_1.speed =  1000; // 0 to 65535 in us between each steps
-    stepper_1.mode = 0; // 0 to full step, 1 to half step, 2 to quarter step, 3 to eighth step, 4 to sixteenth step, 5 to thirty-second step, 6 to sixty-fourth step, 7 to one hundred twenty-eighth step
-    stepper_1.dir = 1; // 0 to go forward, 1 to go backward
-    stepper_1.en = 0; // 0 to enable, 1 to disable
-    XGpio_DiscreteWrite(&stepper_1.SPEED, 1, stepper_1.speed);
-    XGpio_DiscreteWrite(&stepper_1.MODE, 1, stepper_1.mode);
-    XGpio_DiscreteWrite(&stepper_1.DIR, 1, stepper_1.dir);
-    XGpio_DiscreteWrite(&stepper_1.EN, 1, stepper_1.en);
-    XGpio_DiscreteWrite(&stepper_1.STEP, 1, stepper_1.step);
+    switch (init_stepper_state){
+        case 0:
+            /////////////////////////////////////////////////////////////////////////////
+            // init stepper motor
+            /////////////////////////////////////////////////////////////////////////////            
+            // create a stepper object
+            stepper[0] = stepper_1;
+            // initialize the stepper object
+            XGpio_Initialize(&stepper_1.STEP,   STEP_DEVICE_ID);
+            XGpio_Initialize(&stepper_1.SPEED,  SPEED_DEVICE_ID);
+            XGpio_Initialize(&stepper_1.MODE,   MODE_DEVICE_ID);
+            XGpio_Initialize(&stepper_1.DIR,    DIR_DEVICE_ID);
+            XGpio_Initialize(&stepper_1.EN,     EN_DEVICE_ID);
+            XGpio_Initialize(&stepper_1.DONE,   DONE_DEVICE_ID);
+            XGpio_SetDataDirection(&stepper_1.STEP, 1, 0);
+            XGpio_SetDataDirection(&stepper_1.SPEED, 1, 0);
+            XGpio_SetDataDirection(&stepper_1.MODE, 1, 0);
+            XGpio_SetDataDirection(&stepper_1.DIR, 1, 0);
+            XGpio_SetDataDirection(&stepper_1.EN, 1, 0);
+            XGpio_SetDataDirection(&stepper_1.DONE, 1, 1);
+            stepper_1.step = 0; 
+            stepper_1.speed =  1000; // 0 to 65535 in us between each steps
+            stepper_1.mode = 0; // 0 to full step, 1 to half step, 2 to quarter step, 3 to eighth step, 4 to sixteenth step, 5 to thirty-second step, 6 to sixty-fourth step, 7 to one hundred twenty-eighth step
+            stepper_1.dir = 1; // 0 to go forward, 1 to go backward
+            stepper_1.en = 0; // 0 to enable, 1 to disable
+            XGpio_DiscreteWrite(&stepper_1.SPEED, 1, stepper_1.speed);
+            XGpio_DiscreteWrite(&stepper_1.MODE, 1, stepper_1.mode);
+            XGpio_DiscreteWrite(&stepper_1.DIR, 1, stepper_1.dir);
+            XGpio_DiscreteWrite(&stepper_1.EN, 1, stepper_1.en);
+            XGpio_DiscreteWrite(&stepper_1.STEP, 1, stepper_1.step);
+            init_stepper_state = 1;
+            break;
+        case 1:
+            ///////////////////////////////////////////////////////////////////////////////
+            // init endstop switches
+            ///////////////////////////////////////////////////////////////////////////////
+            XGpio_Initialize(&high_switch_elevator, HIGH_SWITCH_ELEVATOR_DEVICE_ID);
+            XGpio_Initialize(&low_switch_elevator, LOW_SWITCH_ELEVATOR_DEVICE_ID);
+            XGpio_SetDataDirection(&high_switch_elevator, 1, 1);
+            XGpio_SetDataDirection(&low_switch_elevator, 1, 1);
+            high_switch_elevator_state = XGpio_DiscreteRead(&high_switch_elevator, 1);
+            low_switch_elevator_state = XGpio_DiscreteRead(&low_switch_elevator, 1);
+            init_stepper_state = 2;
+            break;
+        case 2:
+            ///////////////////////////////////////////////////////////////////////////////
+            // move stepper motor to lower position
+            ///////////////////////////////////////////////////////////////////////////////
+            if (low_switch_elevator_state == 0){
+                stepper_1.en = 1;
+                stepper_1.step += 10;
+                stepper_1.dir = 0;
+                XGpio_DiscreteWrite(&stepper_1.DIR, 1, stepper_1.dir);
+                XGpio_DiscreteWrite(&stepper_1.STEP, 1, stepper_1.step);
+                XGpio_DiscreteWrite(&stepper_1.EN, 1, stepper_1.en);
+            } else {
+                stepper_1.en = 0;
+                stepper_1.step = 0;
+                stepper_1.dir = 0;
+                XGpio_DiscreteWrite(&stepper_1.DIR, 1, stepper_1.dir);
+                XGpio_DiscreteWrite(&stepper_1.STEP, 1, stepper_1.step);
+                XGpio_DiscreteWrite(&stepper_1.EN, 1, stepper_1.en);
+                init_stepper_state = 3;
+            }
+            break;
+    }
 }
 
 uint32_t old_stepper_Timer_ms1 = 0;
@@ -41,21 +91,21 @@ void Stepper_Loop(void){
     if(Timer_ms1 - old_stepper_Timer_ms1 > 3000){
         old_stepper_Timer_ms1 = Timer_ms1;
         // if (XGpio_DiscreteRead(&stepper_1.DONE, 1) == 1){
-        if (stepper_1.dir == 0){
-            stepper_1.en = 0;
-            stepper_1.step = 300;
-            stepper_1.dir = 1;
-            XGpio_DiscreteWrite(&stepper_1.DIR, 1, stepper_1.dir);
-            XGpio_DiscreteWrite(&stepper_1.STEP, 1, stepper_1.step);
-            XGpio_DiscreteWrite(&stepper_1.EN, 1, stepper_1.en);
-        }else{
-            stepper_1.en = 0;
-            stepper_1.step = 300;
-            stepper_1.dir = 0;
-            XGpio_DiscreteWrite(&stepper_1.DIR, 1, stepper_1.dir);
-            XGpio_DiscreteWrite(&stepper_1.STEP, 1, stepper_1.step);
-            XGpio_DiscreteWrite(&stepper_1.EN, 1, stepper_1.en);
-        }
+        // if (stepper_1.dir == 0){
+        //     stepper_1.en = 0;
+        //     stepper_1.step = 300;
+        //     stepper_1.dir = 1;
+        //     XGpio_DiscreteWrite(&stepper_1.DIR, 1, stepper_1.dir);
+        //     XGpio_DiscreteWrite(&stepper_1.STEP, 1, stepper_1.step);
+        //     XGpio_DiscreteWrite(&stepper_1.EN, 1, stepper_1.en);
+        // }else{
+        //     stepper_1.en = 0;
+        //     stepper_1.step = 300;
+        //     stepper_1.dir = 0;
+        //     XGpio_DiscreteWrite(&stepper_1.DIR, 1, stepper_1.dir);
+        //     XGpio_DiscreteWrite(&stepper_1.STEP, 1, stepper_1.step);
+        //     XGpio_DiscreteWrite(&stepper_1.EN, 1, stepper_1.en);
+        // }
     }
 }
 
