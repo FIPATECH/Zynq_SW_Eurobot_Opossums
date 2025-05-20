45eb0aa092a76490840179a7738e30ba73cc1c9a
MartH76 <martin25.husser@gmail.com>
2025-05-20 13:46:34 +0200

    add gestion valve and close them after 1s

--- Eurobot_2025/src/Valve.c
+++ Eurobot_2025/src/Valve.c
@@ -2,6 +2,9 @@
 
 XGpio Valve;
 uint8_t valve_state[4];
+uint8_t previous_valve_state[4];
+uint32_t Timer_valve_on[4];
+
 uint8_t valve_cmd = 0;
 
 void Init_Pump(void){
@@ -9,14 +12,30 @@ void Init_Pump(void){
     XGpio_SetDataDirection(&Valve, 1, 0);
     for (int i = 0; i < 4; i++){
         valve_state[i] = 0;
+        previous_valve_state[i] = 0;
+        Timer_valve_on[i] = 0;
     }
 }
 
-uint32_t old_pump_Timer_ms1 = 0;
+uint32_t old_valve_Timer_ms1 = 0;
+
 
 void Pump_Loop(void){
-    if(Timer_ms1 - old_pump_Timer_ms1 > 10){
-        old_pump_Timer_ms1 = Timer_ms1;
+    if(Timer_ms1 - old_valve_Timer_ms1 > 10){
+        old_valve_Timer_ms1 = Timer_ms1;
+
+        for (int i = 0; i < 4; i++){
+            // check if the valve state has changed and keep track of the time it was turned on
+            if (valve_state[i] != previous_valve_state[i] && valve_state[i] == 1){
+                Timer_valve_on[i] = Timer_ms1;
+            }
+
+            if(Timer_ms1 - Timer_valve_on[i] > 1000 && valve_state[i] == 1){
+                // if the valve has been on for more than 1 second, turn it off
+                valve_state[i] = 0;
+            }
+        }
+
         valve_cmd = valve_state[0] | (valve_state[1] << 1) | (valve_state[2] << 2) | (valve_state[3] << 3);
         XGpio_DiscreteWrite(&Valve, 1, valve_cmd);
     }
