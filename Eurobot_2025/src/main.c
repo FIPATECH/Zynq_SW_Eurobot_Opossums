450d9da9ff63890506c34c3648f0c5a72ea5e730
MartH76 <martin25.husser@gmail.com>
2025-05-22 18:08:37 +0200

    Issue with block and AU test1 => reset asserv during AU

--- Eurobot_2025/src/main.c
+++ Eurobot_2025/src/main.c
@@ -80,7 +80,7 @@ int main()
             init_CAN_MOTOR_variables();
             Init_Pump();
             PWM_Init();
-            // Init_Asserv();
+            Init_Asserv();
         }else{
             // stepper functions
             Init_Stepper_Loop();
