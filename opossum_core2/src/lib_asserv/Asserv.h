761a58dc7b37d9181b9bf9de3c8d11a6acd7d334
MartH76 <martin25.husser@gmail.com>
2025-06-06 21:44:06 +0200

    clean asserv.h

--- opossum_core2/src/lib_asserv/Asserv.h
+++ opossum_core2/src/lib_asserv/Asserv.h
@@ -27,41 +27,144 @@ extern int emergency_break_requested;
 
 /******************************    Fonctions    *******************************/
 
-// initialiser le mode et les differents asservissements
+/**
+ * @brief Initialize the asservissement system.
+ * 
+ */
 void asserv_init(void);
 
 
-// consignes de deplacements du robot
-void motion_block(void) ;
-void motion_free(void) ;
+/**
+ * @brief Block the motion of the robot.
+ * 
+ * This function sets the asservissement mode to break, stopping the robot's movement.
+ */
+void motion_block(void);
+
+/**
+ * @brief Free the motion of the robot.
+ * 
+ * This function sets the asservissement mode to free, allowing the robot to move without constraints.
+ */
+void motion_free(void);
+
+/**
+ * @brief Motion to a specific position.
+ * 
+ * This function sets the robot's position to the specified coordinates and activates the position asservissement mode.
+ * 
+ * @param pos The target position to move to, specified as a Position structure containing x, y, and t (angle).
+ */
 void motion_pos(Position pos);
+
+/**
+ * @brief Disables the asserv of the robot.
+ * 
+ */
 void motion_off(void);
 
+/**
+ * @brief Sets the speed of the robot.
+ * 
+ * This function sets the desired speed of the robot and activates the speed asservissement mode.
+ * 
+ * @param speed The desired speed to set, specified as a Speed structure containing vx, vy, and vt (angular velocity).
+ */
 void motion_speed(Speed speed);
+
+/**
+ * @brief Sets the absolute speed of the robot.
+ * 
+ * This function sets the desired absolute speed of the robot and activates the absolute speed asservissement mode.
+ * 
+ * @param speed The desired absolute speed to set, specified as a Speed structure containing vx, vy, and vt (angular velocity).
+ */
 void motion_absolute_speed(Speed speed);
 
-
-
-// effectue un pas d'asservissement
+/**
+ * @brief Step function for the motion control.
+ * 
+ * This function is called periodically to update the robot's motion based on the current asservissement mode.
+ */
 void motion_step(void);
 
-// fonctions de calcul des commandes en fonction du mode
+/**
+ * @brief Mode off step function.
+ * 
+ */
 void asserv_off_step(void);
+
+/**
+ * @brief Free step function for the asservissement.
+ * 
+ * This function is called when the robot is in free mode, allowing it to move without constraints.
+ */
 void asserv_free_step(void);
 
+/**
+ * @brief Step function for position asservissement.
+ * 
+ * This function calculates the speed orders based on the desired position and current state of the robot.
+ */
 void pos_asserv_step(void);
 
-
+/**
+ * @brief Step function for speed asservissement.
+ * 
+ * This function sets the speed orders based on the desired speed and activates the speed PID.
+ */
 void speed_asserv_step(void);
+
+/**
+ * @brief Step function for emergency break in speed asservissement.
+ * 
+ * This function checks if the robot is moving and applies an emergency break if necessary.
+ */
 void speed_asserv_break_step(void);
+
+/**
+ * @brief Step function for absolute speed asservissement.
+ * 
+ * This function calculates the speed orders based on the desired absolute speed and current robot orientation.
+ */
 void absolute_speed_asserv_step(void);
 
+/**
+ * @brief Checks if the robot is blocked.
+ * 
+ * This function checks if the robot's speed deviates significantly from the desired speed and handles the blocked state.
+ * 
+ * @param period The time period over which to check for blockage.
+ */
 void asserv_check_blocked(float period);
 
-// indique si l'asservissement en cours a termine
+/**
+ * @brief Get the status of the asservissement.
+ * 
+ * This function returns 1 if the asservissement is done (in OFF mode), otherwise returns 0.
+ * 
+ * @return int Returns 1 if asservissement is done, otherwise 0.
+ */
 int Get_asserv_done();
 
+/**
+ * @brief Calculates the radial speed based on the distance to the target.
+ * 
+ * This function computes the radial speed required to reach a target distance using a predefined maximum acceleration.
+ * 
+ * @param distance The distance to the target in meters.
+ * @return float Returns the calculated radial speed in m/s.
+ */
 float radial_speed_calculation(float distance);
+
+/**
+ * @brief Calculates the angular speed based on the angle to the target.
+ * 
+ * This function computes the angular speed required to reach a target angle using a predefined maximum angular acceleration.
+ * 
+ * @param angle The angle to the target in radians.
+ * @return float Returns the calculated angular speed in rad/s.
+ */
 float angular_speed_calculation(float angle);
 
 #endif // _ASSERV_H_
