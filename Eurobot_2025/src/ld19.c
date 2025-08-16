4c56fbe84df0ec602b62b5e992bba2ad84acaa12
MartH76 <martin25.husser@gmail.com>
2025-08-16 14:56:34 +0200

    continue work

--- Eurobot_2025/src/ld19.c
+++ Eurobot_2025/src/ld19.c
@@ -14,20 +14,25 @@ void initLD19Instance(LD19Instance *instance) {
     initLiDARFrame(&instance->frame);
     initLiDARFilter(&instance->filter);
     instance->data_handler.index = 0;
+
+    instance->en_crc_check = 1;
+    instance->en_full_scan = 1;
+    instance->en_filtering = 1;
+    instance->set_upside_down = 0;
 }
 
-void initLiDARFrame(LiDARFrameTypeDef *frame) {
-    frame->header = HEADER;
-    frame->ver_len = sizeof(LiDARFrameTypeDef);
-    frame->speed = 0;
-    frame->start_angle = 0;
-    for (int i = 0; i < LD19_POINT_PER_PACK; i++) {
-        frame->point[i].distance = 0;
-        frame->point[i].intensity = 0;
+void initLiDARFrame(LD19PacketHandler *frame) {
+    frame->packet.header = LD19_HEADER;
+    frame->packet.ver_len = sizeof(LD19PacketHandler);
+    frame->packet.speed = 0;
+    frame->packet.start_angle = 0;
+    for (int i = 0; i < LD19_PACKET_SIZE; i++) {
+        frame->packet.point[i].distance = 0;
+        frame->packet.point[i].intensity = 0;
     }
-    frame->end_angle = 0;
-    frame->timestamp = 0;
-    frame->crc8 = 0;
+    frame->packet.end_angle = 0;
+    frame->packet.timestamp = 0;
+    frame->packet.crc8 = 0;
 }
 
 void initLiDARFilter(LD19Filter *filter) {
@@ -68,5 +73,96 @@ void setAngleRange(LD19Filter *filter, float min_angle, float max_angle) {
     filter->max_angle = max_angle;
 }
 
+void enableCRCCheck(LD19Instance *instance) {
+    instance->en_crc_check = 1;
+}
+
+void disableCRCCheck(LD19Instance *instance) {
+    instance->en_crc_check = 0;
+}
+
+void enableFullScan(LD19Instance *instance) {
+    instance->en_full_scan = 1;
+}
+
+void disableFullScan(LD19Instance *instance) {
+    instance->en_full_scan = 0;
+}
+
+void enableFiltering(LD19Instance *instance) {
+    instance->en_filtering = 1;
+}
+
+void disableFiltering(LD19Instance *instance) {
+    instance->en_filtering = 0;
+}
+
+void turnUpsideDown(LD19Instance *instance) {
+    instance->set_upside_down = 1;
+}
+
+void turnRightSideUp(LD19Instance *instance) {
+    instance->set_upside_down = 0;
+}
 
 
+uint8_t ld19_readData(LD19Instance *instance, XUartLite *UartLite) {
+    return instance->en_crc_check ? ld19_readDataCRC(instance, UartLite) : ld19_readDataNoCRC(instance, UartLite);
+}
+
+uint8_t ld19_readDataCRC(LD19Instance *instance, XUartLite *UartLite) {
+    // Read data from the LiDAR sensor and process it
+    uint8_t current, result = 0;
+    if (XUartLite_Recv(UartLite, &current, 1)) {
+        // Process the received data
+        if((instance->receivedData.index > 1) || 
+                    (instance->receivedData.index == 0 && current == LD19_HEADER) ||
+                    (instance->receivedData.index == 1 && current == LD19_VER_SIZE)) {
+                        
+            instance->receivedData.packet.bytes[instance->receivedData.index] = current;
+            if(instance->receivedData.index < LD19_PACKET_SIZE - 1) { 
+                instance->receivedData.computedCRC = CrcTable[instance->receivedData.computedCRC ^ current];
+                instance->receivedData.index++;
+            }else{
+                if(instance->receivedData.computedCRC == current) {
+                    ld19_computeData(instance);
+                    result = 1;
+                }else{
+                    instance->receivedData.checksumFailCount++;
+                }
+                instance->receivedData.index = 0;
+                instance->receivedData.computedCRC = 0;
+            }
+        }
+    }
+    return result;
+}
+
+uint8_t ld19_readDataNoCRC(LD19Instance *instance, XUartLite *UartLite) {
+    // Read data from the LiDAR sensor without CRC check
+    uint8_t current, result = 0;
+    if (XUartLite_Recv(UartLite, &current, 1)) {
+        // Process the received data
+        if((instance->receivedData.index > 1) || 
+                    (instance->receivedData.index == 0 && current == LD19_HEADER) ||
+                    (instance->receivedData.index == 1 && current == LD19_VER_SIZE)) {
+
+            instance->receivedData.packet.bytes[instance->receivedData.index] = current;
+            if(instance->receivedData.index < LD19_PACKET_SIZE - 1) {
+                instance->receivedData.index++;
+            }else{
+                ld19_computeData(instance);
+                result = 1;
+                instance->receivedData.index = 0;
+            }
+        }
+    }
+    return result;
+}
+
+void ld19_readscan(LD19Instance *instance, XUartLite *UartLite) {
+    // TODO write function to build a full scan from the received packets
+}
+
+void ld19_computeData(LD19Instance *instance) {
+}
--- Eurobot_2025/src/ld19.h
+++ Eurobot_2025/src/ld19.h
@@ -1,10 +1,11 @@
 #ifndef LD19_H
 #define LD19_H
 
-#define HEADER 0x54
+#define LD19_HEADER 0x54
+#define LD19_VER_SIZE 0x2C
 
-#define LD19_POINT_PER_PACK 12
-#define LD19_FRAME_SIZE 47 // 1(header) + 1(ver_len) + 2(speed) + 2(start_angle) + 12*3(point) + 2(end_angle) + 2(timestamp) + 1(crc8)
+#define LD19_PTS_PER_PACKETS 12
+#define LD19_PACKET_SIZE 47 // 1(header) + 1(ver_len) + 2(speed) + 2(start_angle) + 12*3(point) + 2(end_angle) + 2(timestamp) + 1(crc8)
 #define LD19_DATA_SIZE 3
 
 #define LD19_MAX_PTS_SCAN 1200 
@@ -19,11 +20,12 @@ typedef struct {
     uint8_t  ver_len; 
     uint16_t speed; 
     uint16_t start_angle; 
-    LidarPointStructDef point[LD19_POINT_PER_PACK]; 
+    LidarPointStructDef point[LD19_PTS_PER_PACKETS]; 
     uint16_t end_angle; 
     uint16_t timestamp; 
-    uint8_t   crc8; 
-} LiDARFrameTypeDef;
+    uint8_t crc8; 
+    uint8_t bytes[LD19_PACKET_SIZE]
+} LD19Packet;
 
 uint8_t CrcTable[256] = {
   0x00, 0x4d, 0x9a, 0xd7, 0x79, 0x34, 0xe3,
@@ -62,7 +64,16 @@ typedef struct {
 typedef struct  {
   DataPoint points[LD19_MAX_PTS_SCAN];
   uint16_t index;
-} DataPointHandler;
+} LD19DataPointHandler;
+
+typedef struct {
+    LD19Packet packet;
+    uint8_t computedCRC;
+    uint8_t index;
+
+    uint8_t checksumFailCount; // Count of checksum failures
+} LD19PacketHandler;
+
 
 typedef struct {
     uint16_t min_distance;
@@ -73,17 +84,19 @@ typedef struct {
 } LD19Filter;
 
 typedef struct {
-    LiDARFrameTypeDef frame;
+    LD19PacketHandler receivedData;
     LD19Filter filter;
-    DataPointHandler data_handler;
+    LD19DataPointHandler dataHandler;
+    uint8_t en_crc_check; // Enable CRC check
+    uint8_t en_full_scan; // return true only when a full 360° scan is available
+    uint8_t en_filtering; // Enable filtering
+    uint8_t set_upside_down; // Set upside down (1: upside down)
 }LD19Instance;
-
 
 uint8_t CalCRC8(uint8_t *p, uint8_t len);
 
-
 void initLD19Instance(LD19Instance *instance);
-void initLiDARFrame(LiDARFrameTypeDef *frame);
+void initLiDARFrame(LD19PacketHandler *frame);
 void initLiDARFilter(LD19Filter *filter);
 
 void setIntensityThreshold(LD19Filter *filter, uint8_t threshold);
@@ -94,6 +107,20 @@ void setAngleRange(LD19Filter *filter, float min_angle, float max_angle);
 void setMinAngle(LD19Filter *filter, float min_angle);
 void setMaxAngle(LD19Filter *filter, float max_angle);
 
+void enableCRCCheck(LD19Instance *instance);
+void disableCRCCheck(LD19Instance *instance);
+void enableFullScan(LD19Instance *instance);
+void disableFullScan(LD19Instance *instance);
+void enableFiltering(LD19Instance *instance);
+void disableFiltering(LD19Instance *instance);
+void turnUpsideDown(LD19Instance *instance);
+void turnRightSideUp(LD19Instance *instance);
 
+
+uint8_t ld19_readData(LD19Instance *instance, XUartLite *UartLite);
+uint8_t ld19_readDataCRC(LD19Instance *instance, XUartLite *UartLite);
+uint8_t ld19_readDataNoCRC(LD19Instance *instance, XUartLite *UartLite);
+
+void ld19_readscan(LD19Instance *instance, XUartLite *UartLite);
 
 #endif // LD19_H
