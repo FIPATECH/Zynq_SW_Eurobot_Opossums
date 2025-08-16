cc3d9131e8b26b254a94a936ec3ae94619b1c7d5
MartH76 <martin25.husser@gmail.com>
2025-08-16 21:05:46 +0200

    update conversion lib

--- Eurobot_2025/src/ld19.c
+++ Eurobot_2025/src/ld19.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "ld19crc.h"
 
 
 uint8_t CalCRC8(uint8_t *p, uint8_t len){ 
@@ -19,6 +20,9 @@ void initLD19Instance(LD19Instance *instance) {
     instance->en_full_scan = 1;
     instance->en_filtering = 1;
     instance->set_upside_down = 0;
+
+    instance->currentScan = &instance->scanA;
+    instance->previousScan = &instance->scanB;
 }
 
 void initLiDARFrame(LD19PacketHandler *frame) {
@@ -113,7 +117,7 @@ uint8_t ld19_readData(LD19Instance *instance, XUartLite *UartLite) {
 uint8_t ld19_readDataCRC(LD19Instance *instance, XUartLite *UartLite) {
     // Read data from the LiDAR sensor and process it
     uint8_t current, result = 0;
-    if (XUartLite_Recv(UartLite, &current, 1)) {
+    while (XUartLite_Recv(UartLite, &current, 1)) {
         // Process the received data
         if((instance->receivedData.index > 1) || 
                     (instance->receivedData.index == 0 && current == LD19_HEADER) ||
@@ -141,7 +145,7 @@ uint8_t ld19_readDataCRC(LD19Instance *instance, XUartLite *UartLite) {
 uint8_t ld19_readDataNoCRC(LD19Instance *instance, XUartLite *UartLite) {
     // Read data from the LiDAR sensor without CRC check
     uint8_t current, result = 0;
-    if (XUartLite_Recv(UartLite, &current, 1)) {
+    while (XUartLite_Recv(UartLite, &current, 1)) {
         // Process the received data
         if((instance->receivedData.index > 1) || 
                     (instance->receivedData.index == 0 && current == LD19_HEADER) ||
@@ -161,8 +165,80 @@ uint8_t ld19_readDataNoCRC(LD19Instance *instance, XUartLite *UartLite) {
 }
 
 void ld19_readscan(LD19Instance *instance, XUartLite *UartLite) {
-    // TODO write function to build a full scan from the received packets
+    static uint8_t isInit = 0;
+    static float lastAngle = 0;
+    static float startAngle = 0;
+    DataPoint data;
+    uint8_t result = 0;
+
+    if(ld19_readData(instance, UartLite)) {
+        for(int i = 0; i < LD19_PTS_PER_PACKETS; i++) {
+            if (instance->receivedData.packet.angles[i] < lastAngle) {
+                if (!isInit) {
+                  isInit = 1;
+                } else {
+                  if (lastAngle - startAngle > 340) {  // TODO check if this condition is enough to prevent partial scan prints
+                    result = 1;
+                  }
+                  startAngle = instance->receivedData.packet.angles[i];
+
+                  if (instance->en_full_scan) {
+                    swapBuffers();
+                  }
+                }
+            }
+            lastAngle = instance->receivedData.packet.angles[i];
+            if (instance->currentScan->index < LD19_MAX_PTS_SCAN) {
+                data.angle = instance->receivedData.packet.angles[i];
+                data.distance = instance->receivedData.packet.point[i].distance;
+                data.intensity = instance->receivedData.packet.point[i].intensity;
+                if (!instance->en_filtering || filter(data)) {
+                    data.x = instance->xPosition + instance->xOffset * cos(instance->angularPosition) - instance->yOffset * sin(instance->angularPosition) + data.distance * cos((data.angle + instance->angularPosition + instance->angularOffset) * PI / 180);
+                    data.y = instance->yPosition + instance->xOffset * sin(instance->angularPosition) + instance->yOffset * cos(instance->angularPosition) - data.distance * sin((data.angle + instance->angularPosition + instance->angularOffset) * PI / 180);
+                    instance->currentScan->points[instance->currentScan->index] = data;
+                    instance->currentScan->index++;
+                }
+            }
+        }
+        if (!instance->en_full_scan) {
+          ld19_swapBuffers(instance);
+        }
+    } 
+    return result;
 }
 
 void ld19_computeData(LD19Instance *instance) {
+    float angleStep = ld19_getAngleStep(instance);
+    if (angleStep > LD19_ANGLE_STEP_MAX)  // Should not be possible
+      return;
+
+    int8_t reverse = (instance->set_upside_down ? -1 : 1);
+    float fsa = instance->receivedData.packet.startAngle / 100.0;
+    for (uint16_t i = 0; i < LD19_PTS_PER_PACKETS; i++) {
+      float raw_deg = fsa + i * angleStep;
+      instance->receivedData.packet.angles[i] = (raw_deg <= 360 ? raw_deg : raw_deg - 360) * reverse;
+    }
+}
+
+
+float ld19_getAngleStep(LD19Instance *instance) {
+    float fsa = (float)(instance->receivedData.packet.startAngle) / 100.0;
+    float lsa = (float)(instance->receivedData.packet.lastAngle) / 100.0;
+    float range = lsa - fsa;
+    if (range < 0) {
+        range += 360;
+    }
+    return range / (LD19_PTS_PER_PACKETS - 1);
+}
+
+void ld19_swapBuffers(LD19Instance *instance) {
+  if (instance->currentBuffer) {
+    instance->currentScan = &instance->scanB;
+    instance->previousScan = &instance->scanA;
+  } else {
+    instance->currentScan = &instance->scanA;
+    instance->previousScan = &instance->scanB;
+  }
+  instance->currentBuffer = !instance->currentBuffer;
+  instance->currentScan->index = 0;
 }
